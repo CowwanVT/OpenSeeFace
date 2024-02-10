b636b0452cb17eacbf192085ce11f4d00738db2e
Cowwan <CowwanVT@gmail.com>
2024-02-10 12:31:49 -0500

    a

--- vts.py
+++ vts.py
@@ -2,7 +2,6 @@ import time
 import socket
 import struct
 
-
 class VTS():
 
     features = ["eye_l", "eye_r", "eyebrow_steepness_l", "eyebrow_updown_l", "eyebrow_quirk_l", "eyebrow_steepness_r", "eyebrow_updown_r", "eyebrow_quirk_r", "mouth_corner_updown_l", "mouth_corner_inout_l", "mouth_corner_updown_r", "mouth_corner_inout_r", "mouth_open", "mouth_wide"]
