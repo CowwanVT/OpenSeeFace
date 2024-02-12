worked on readability fixed standard deviation formula for eyes updated eye confidence limitations to reject the eye movement entirely because it's a very very small portion of eye movements

eyes.py
@@ -19,15 +19,24 @@ def clamp_to_im(pt, w, h):
 
 def rotate(origin, point, a):
     x, y = point - origin
-    qx = origin[0] + math.cos(-a) * x - math.sin(-a) * y
-    qy = origin[1] + math.sin(-a) * x + math.cos(-a) * y
+    a = -a
+
+    qx = math.cos(a) * x - math.sin(a) * y
+
+    qy = math.sin(a) * x + math.cos(a) * y
+
+    qx+= origin[0]
+    qy+= origin[1]
+
     return qx, qy
 
 def clamp (value, minimum, maxium):
     return max(min(value,maxium),minimum)
 
 def rotate_image(image, a, center): #twice per frame, 0.2ms - 0.25ms each, improved to 0.15ms - 0.25ms
-    M = cv2.getRotationMatrix2D((float(center[0]), float(center[1])), math.degrees(a), 1.0)
+    center = (float(center[0]), float(center[1]))
+    a = math.degrees(a)
+    M = cv2.getRotationMatrix2D(center,a, 1.0)
     (h, w) = image.shape[:2]
     image = cv2.warpAffine(image, M, (w, h), flags = cv2.INTER_CUBIC)
     return image
@@ -53,7 +62,8 @@ class Eye():
     def prepare_eye(self, faceFrame):
         self.state = [1.,0.,0.,0.]
         im = faceFrame
-        (x1, y1), (x2, y2), a = self.corners_to_eye( im.shape[1], im.shape[0])
+        (x1, y1), (x2, y2), a = self.corners_to_eye(im.shape)
+
         #rotating an image is expensive and reduces clarity
         #so I just don't if it's a relatively small angle
         if math.degrees(a) > 7.5 and math.degrees(a) < 352.5:
@@ -76,7 +86,9 @@ class Eye():
         self.info = [x1, y1, scale, self.outerPoint, a]
         return
 
-    def corners_to_eye(self, w, h):
+    def corners_to_eye(self, shape):
+
+        h, w, _ = shape
         c1 = np.array(self.outerPoint)
         c2 = np.array(self.innerPoint)
 
@@ -111,14 +123,11 @@ class Eye():
         eye_y = 4.0 * (y + off_y)
 
         #I'm proud of this
-        #if eye movements are below 2 standard deviations of the average the movement is severely limited
+        #if eye movements are below 2 standard deviations of the average the movement rejected
         #the eyes are handled independenly because my testing showed that one eye tended to have a higher condifence than the other
-        limit = max((self.results[0][x,y]- 0.5)*(2/confidenceThreshold),0)
         if self.results[0][x,y] < confidenceThreshold:
-            Delta = eye_y - self.lastEyeState[0]
-            eye_y = self.lastEyeState[0] + clamp(Delta, -limit, limit)
-            Delta = eye_x - self.lastEyeState[1]
-            eye_x = self.lastEyeState[1] + clamp(Delta, -limit, limit)
+            eye_y = self.lastEyeState[0]
+            eye_x = self.lastEyeState[1]
 
         self.lastEyeState = [eye_y, eye_x]
 
@@ -136,13 +145,19 @@ class Eye():
         return
 
     def calculateStandardDeviation(self):
+        #calculates a *vague* approximation of the standard deviation of eye condivences
+        #allows me to reject a very small portion of eye movements while preventing errant eye movements
         self.eye_tracking_frames+=1
 
         avgRatio = 1/self.eye_tracking_frames
-        self.averageEyeConfidence = (self.averageEyeConfidence*(1-avgRatio))+ (self.condifence * avgRatio)
 
-        avgRatio = 1/(self.eye_tracking_frames + 1)
-        self.averageEyeConfidenceVariance  = pow(((self.condifence - self.averageEyeConfidence) * avgRatio) + (self.averageEyeConfidenceVariance  * (1-avgRatio)), 2)
+        self.averageEyeConfidence *= (1-avgRatio)
+        self.averageEyeConfidence += self.condifence * avgRatio
+
+        self.averageEyeConfidenceVariance *= 1-avgRatio
+
+        self.averageEyeConfidenceVariance += pow(self.condifence - self.averageEyeConfidence, 2) * avgRatio
+
         self.standardDeviation = math.sqrt(self.averageEyeConfidenceVariance)
 
 class EyeTracker():

landmarks.py
@@ -47,7 +47,7 @@ def landmarks(tensor, crop_info):
     return (np.average(t_conf), lms)
 
 def estimate_depth( face, width, height):
-    camera = np.array([[width, 0, width/2], [0, width, height/2], [0, 0, 1]], np.float32)
+    camera = np.array([[width, 0, width/2], [0, width, height/2], [0, 0, 1]])
     inverse_camera = np.linalg.inv(camera)
     lms = np.concatenate((face.lms, np.array(face.eye_state)[0:2,1:4]), 0)
     image_pts = lms[face.contourPoints, 0:2]
@@ -67,18 +67,55 @@ def estimate_depth( face, width, height):
         return False, np.zeros(4), np.zeros(3), 99999., pts_3d, lms
 
     rmat, _ = cv2.Rodrigues(face.rotation)
-    inverse_rotation = np.linalg.inv(rmat)
+
     t_reference = face.face_3d.dot(rmat.transpose())
     t_reference = t_reference + face.translation
     t_reference = t_reference.dot(camera.transpose())
+
     t_depth = t_reference[:, 2]
     t_depth[t_depth == 0] = 0.000001
     t_depth_e = np.expand_dims(t_depth[:],1)
     t_reference = t_reference[:] / t_depth_e
+
+    inverse_rotation = np.linalg.inv(rmat)
+
     pts_3d[0:66] = np.stack([lms[0:66,0], lms[0:66,1], np.ones((66,))], 1)
     pts_3d[0:66] = pts_3d[0:66] * t_depth_e[0:66]
+    pts_3d[0:66] = pts_3d[0:66].dot(inverse_camera.transpose())
+    pts_3d[0:66] = pts_3d[0:66] - face.translation
+    pts_3d[0:66] = pts_3d[0:66].dot(inverse_rotation.transpose())
 
-    pts_3d[0:66] = (pts_3d[0:66].dot(inverse_camera.transpose()) - face.translation).dot(inverse_rotation.transpose())
+    d1 = np.linalg.norm(lms[66,0:2] - lms[36,0:2])
+    d2 = np.linalg.norm(lms[66,0:2] - lms[39,0:2])
+    d = d1 + d2
+    pt = (pts_3d[36] * d1 + pts_3d[39] * d2) / d
+
+    reference = rmat.dot(pt)
+    reference = reference + face.translation
+    reference = camera.dot(reference)
+    depth = reference[2]
+
+    pt_3d = [lms[66][0] * depth, lms[66][1] * depth, depth]
+    pt_3d = inverse_camera.dot(pt_3d)
+    pt_3d = pt_3d - face.translation
+    pt_3d = inverse_rotation.dot(pt_3d)
+    pts_3d[66,:] = pt_3d[:]
+
+    d1 = np.linalg.norm(lms[67,0:2] - lms[42,0:2])
+    d2 = np.linalg.norm(lms[67,0:2] - lms[45,0:2])
+    d = d1 + d2
+    pt = (pts_3d[42] * d1 + pts_3d[45] * d2) / d
+
+    reference = rmat.dot(pt)
+    reference = reference + face.translation
+    reference = camera.dot(reference)
+    depth = reference[2]
+
+    pt_3d = [lms[67][0] * depth, lms[67][1] * depth, depth]
+    pt_3d = inverse_camera.dot(pt_3d)
+    pt_3d = pt_3d - face.translation
+    pt_3d = inverse_rotation.dot(pt_3d)
+    pts_3d[67,:] = pt_3d[:]
 
     # Right eyeball
     # Eyeballs have an average diameter of 12.5mm
@@ -97,41 +134,14 @@ def estimate_depth( face, width, height):
     eye_center[2]-=depth
     pts_3d[69] = eye_center
 
-    d1 = np.linalg.norm(lms[66,0:2] - lms[36,0:2])
-    d2 = np.linalg.norm(lms[66,0:2] - lms[39,0:2])
-    d = d1 + d2
-    pt = (pts_3d[36] * d1 + pts_3d[39] * d2) / d
+    pts_3d[np.isnan(pts_3d).any(axis=1)] = np.array([0.,0.,0.])
 
-    reference = rmat.dot(pt)
-    reference = reference + face.translation
-    reference = camera.dot(reference)
-    depth = reference[2]
-    pt_3d = [lms[66][0] * depth, lms[66][1] * depth, depth]
-    pt_3d = inverse_camera.dot(pt_3d)
-    pt_3d = pt_3d - face.translation
-    pt_3d = inverse_rotation.dot(pt_3d)
-    pts_3d[66,:] = pt_3d[:]
-
-    d1 = np.linalg.norm(lms[67,0:2] - lms[42,0:2])
-    d2 = np.linalg.norm(lms[67,0:2] - lms[45,0:2])
-    d = d1 + d2
-    pt = (pts_3d[42] * d1 + pts_3d[45] * d2) / d
-
-    reference = rmat.dot(pt)
-    reference = reference + face.translation
-    reference = camera.dot(reference)
-    depth = reference[2]
-    pt_3d = [lms[67][0] * depth, lms[67][1] * depth, depth]
-    pt_3d = inverse_camera.dot(pt_3d)
-    pt_3d = pt_3d - face.translation
-    pt_3d = inverse_rotation.dot(pt_3d)
-    pts_3d[67,:] = pt_3d[:]
-
-    pts_3d[np.isnan(pts_3d).any(axis=1)] = np.array([0.,0.,0.], dtype=np.float32)
     pnp_error = np.power(lms[0:17,0:2] - t_reference[0:17,0:2], 2).sum()
     pnp_error += np.power(lms[30,0:2] - t_reference[30,0:2], 2).sum()
+
     if np.isnan(pnp_error):
         pnp_error = 9999999.
+
     pnp_error = math.sqrt(pnp_error / (2.0 * image_pts.shape[0]))
     if pnp_error > 300:
         face.fail_count += 1