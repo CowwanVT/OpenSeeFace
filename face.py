fixed head rotation, cleaned up code

face.py
@@ -1,6 +1,5 @@
 import numpy as np
 import math
-import featureExtractor
 import apiFeatureExtractor
 
 class FaceInfo():
@@ -81,11 +80,10 @@ class FaceInfo():
     def __init__(self):
         self.reset()
         self.alive = False
-        self.coord = None
         self.base_scale_v = self.face_3d[27:30, 1] - self.face_3d[28:31, 1]
         self.base_scale_h = np.abs(self.face_3d[[0, 36, 42], 0] - self.face_3d[[16, 39, 45], 0])
         self.fail_count = 0
-        self.frameNumber = 0
+        self.headRotation = None
 
     def reset(self):
         self.alive = False
@@ -95,26 +93,17 @@ class FaceInfo():
         self.rotation = np.array([0.0, 0.0, 0.0])
         self.translation = np.array([0.0, 0.0, 0.0])
         self.success = None
-        self.quaternion = None
-        self.euler = None
         self.pts_3d = None
-        self.eye_blink = None
         self.pnp_error = 0
-        self.features = featureExtractor.FeatureExtractor()
-
-
         self.apiFeatures = apiFeatureExtractor.APIfeatureExtractor()
         self.currentAPIFeatures = {}
-
-        self.current_features = {}
         self.contour = self.face_3d[self.contourPoints]
 
-    def update(self, result, coord):
+    def update(self, result):
         if result is None:
             self.reset()
         else:
             self.conf, (self.lms, self.eye_state) = result
-            self.coord = coord
             self.alive = True
 
     def normalize_pts3d(self, pts_3d):
@@ -122,7 +111,6 @@ class FaceInfo():
         pts_3d[:, 0:2] -= pts_3d[30, 0:2]
         alpha = math.atan2(*((pts_3d[27, 0:2]) - pts_3d[30, 0:2])[::-1])
         alpha = alpha % (2*math.pi)
-
         cosalpha = math.cos(alpha-(math.pi/2))
         sinalpha = math.sin(alpha-(math.pi/2))
 
@@ -138,15 +126,9 @@ class FaceInfo():
         return pts_3d
 
     def adjust_3d(self):
-        self.frameNumber += 1
         if self.conf < 0.6 or self.pnp_error > 300:
             return
 
         self.pts_3d = self.normalize_pts3d(self.pts_3d)
-        if self.frameNumber < 30:
-            return
-        self.current_features = self.features.update(self.pts_3d)
-        self.currentAPIFeatures = self.apiFeatures.update(self.pts_3d, self.rotation)
-        self.eye_blink = []
-        self.eye_blink.append(1 - min(max(-0.1, -self.current_features["eye_r"]), 1.1))
-        self.eye_blink.append(1 - min(max(-0.1, -self.current_features["eye_l"]), 1.1))
+        self.currentAPIFeatures = self.apiFeatures.update(self.pts_3d, self.headRotation)
+

landmarks.py
@@ -1,25 +1,7 @@
 import numpy as np
 import cv2
+import math
 cv2.setNumThreads(6)
-
-def matrix_to_quaternion(m):
-    t = 0.0
-    q = [0.0, 0.0, 0, 0.0]
-    if m[2,2] < 0:
-        if m[0,0] > m[1,1]:
-            t = 1 + m[0,0] - m[1,1] - m[2,2]
-            q = [t, m[0,1]+m[1,0], m[2,0]+m[0,2], m[1,2]-m[2,1]]
-        else:
-            t = 1 - m[0,0] + m[1,1] - m[2,2]
-            q = [m[0,1]+m[1,0], t, m[1,2]+m[2,1], m[2,0]-m[0,2]]
-    else:
-        if m[0,0] < -m[1,1]:
-            t = 1 - m[0,0] - m[1,1] + m[2,2]
-            q = [m[2,0]+m[0,2], m[1,2]+m[2,1], t, m[0,1]-m[1,0]]
-        else:
-            t = 1 + m[0,0] + m[1,1] + m[2,2]
-            q = [m[1,2]-m[2,1], m[2,0]-m[0,2], m[0,1]-m[1,0], t]
-    return np.array(q) / (pow(t, 0.5)* 0.5)
 
 def landmarks(tensor, crop_info):
     crop_x1, crop_y1, scale_x, scale_y = crop_info
@@ -141,6 +123,13 @@ def estimate_depth( face, width, height):
 
     rmat, _ = cv2.Rodrigues(face.rotation)
 
+    rotationScale = math.sqrt(rmat[1][2] * rmat[1][2] + rmat[2][2]* rmat[2][2])
+    xAxis = math.asin(rmat[1][2]/rotationScale)*180/3.14
+    zAxis =math.atan2(rmat[0][1], rmat[0][0])*180/3.14
+    yAxis =math.atan2(-rmat[0][2],rotationScale)*180/3.14
+    face.headRotation = [xAxis, yAxis, zAxis]
+
+
     t_reference = face.face_3d.dot(rmat.transpose())
     t_reference = t_reference + face.translation
     t_reference = t_reference.dot(camera.transpose())
@@ -172,11 +161,7 @@ def estimate_depth( face, width, height):
     else:
         face.fail_count = 0
 
-    euler = cv2.RQDecomp3x3(rmat)[0]
-
     face.success = True
-    face.quaternion = matrix_to_quaternion(rmat)
-    face.euler = euler
     face.pnp_error = pnp_error
     face.pts_3d = pts_3d
     face.lms = lms

tracker.py
@@ -153,7 +153,7 @@ class Tracker():
             return self.early_exit("Confidence below threshold", start)
 
         eye_state = self.EyeTracker.get_eye_state(self.model, frame, lms)
-        self.face_info.update((confidence, (lms, eye_state)), np.array(lms)[:, 0:2].mean(0))
+        self.face_info.update((confidence, (lms, eye_state)))
 
 
         duration_model = 1000 * (time.perf_counter() - start_model)